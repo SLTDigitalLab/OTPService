1453992fc3eaeedf9fdc61d85a5fab5abbfa2933
Navindu Dananga <navindudananga123@gmail.com>
2023-10-08 02:22:15 +0530

    management service configured

---
+++ app/api/v1/management/tenent.py
@@ -0,0 +1,63 @@
+from db.psql_connector import DB, default_config
+from type_def.tenent import Tenent, TenentRq
+from type_def.auth import User
+from type_def.common import Success, Error
+from management.tenents import Tenents
+
+class TenentAPI(Tenents):
+    def __init__(self, user: User) -> None:
+        super().__init__(user)
+
+    def new(self, t: TenentRq) -> Tenent:
+        tenent : Tenent | None = super().new(t)
+        if tenent:
+            return  Success("Tenent created successfully", 200, tenent.__dict__)
+        return Error("Failed to create tenent", 1000, 400)
+
+    def get(self, id):
+        tenent: Tenent | None = super().get(id)
+        if tenent:
+            return Success("Tenent found", 200, tenent.__dict__)
+        return Error("Tenent not found", 1000, 400)
+    
+    def list(self):
+        tenent_list: list[Tenent] | None = super().list()
+        if tenent_list:
+            return Success("Tenent found", 200, [_r.__dict__ for _r in tenent_list])
+        return Error("Tenent not found", 1000, 400)
+
+    def update():
+        pass
+
+    def delete():
+        pass
+
+    def rename(self, id: str, name: str):
+        super().rename(id, name)
+        # self.db.exec("UPDATE tenents SET name = %s, verified = %s WHERE tenent_id = %s and user_id = %s", (name, False, id, self.user.id))
+        # c = self.db.commit()
+        # print(c)
+
+    # def verify(self, user):
+    #     super().verify()
+    #     self.db.exec("UPDATE tenents SET verified = %s WHERE tenent_id = %s ", (True, self.user.id))
+    #     self.db.commit()
+        
+    # def disable(self):
+    #     self.db.exec("UPDATE tenents SET disabled = %s WHERE user_id = %s", (True, self.user.id))
+    #     self.db.commit()
+
+
+class TenentAPIAdmin:
+    def __init__(user: User) -> None:
+        pass
+
+    def verify():
+        pass
+
+    def disable():
+        pass
+
+    def delete():
+        pass
+
---
+++ app/management/tenents.py
@@ -0,0 +1,81 @@
+from type_def.common import Error, Success
+from type_def.tenent import Tenent, TenentRq
+from db.psql_connector import DB, default_config
+from type_def.auth import User
+
+
+class Tenents:
+    db: DB = DB(default_config())
+    user: User = None
+
+    def __init__(self, user: User) -> None:
+        self.user = user
+
+    def new(self, t: TenentRq) -> Tenent | None:
+        print(self.user)
+        if self.user:
+            self.db.exec(
+                "INSERT INTO tenents (name, user_id, email_otp_size, email_otp_expired_in_s, sms_otp_size, sms_otp_expired_in_s, sms_otp_template, email_otp_template) VALUES (%s, %s, %s, %s, %s, %s, %s, %s) RETURNING *",
+                (
+                    t.name,
+                    self.user.id,
+                    t.email_otp_size,
+                    t.email_otp_expired_in_s,
+                    t.sms_otp_size,
+                    t.sms_otp_expired_in_s,
+                    t.sms_otp_template,
+                    t.email_otp_template
+                ),
+            )
+            self.db.commit()
+            result = self.db.fetchone()
+            if result:
+                return Tenent(**result)
+        return None
+
+    def get(self, id) -> Tenent | None:
+        self.db.exec("SELECT * FROM tenents WHERE user_id = %s and id = %s LIMIT 1", (self.user.id, id))
+        result = self.db.fetchone()
+        if result:
+            return Tenent(**result)
+        return None
+    
+    def list(self) -> list[Tenent] | None :
+        self.db.exec("SELECT * FROM tenents WHERE user_id = %s", (self.user.id,))
+        result = self.db.fetchall()
+        if result:
+            return [Tenent(**r) for r in result]
+        return None
+
+    def update():
+        pass
+
+    def delete():
+        pass
+
+    def rename(self, id: str, name: str):
+        self.db.exec("UPDATE tenents SET name = %s, verified = %s WHERE tenent_id = %s and user_id = %s", (name, False, id, self.user.id))
+        c = self.db.commit()
+        print(c)
+
+    def verify(self):
+        self.db.exec("UPDATE tenents SET verified = %s WHERE user_id = %s", (True, self.user.id))
+        self.db.commit()
+        
+    def disable(self):
+        self.db.exec("UPDATE tenents SET disabled = %s WHERE user_id = %s", (True, self.user.id))
+        self.db.commit()
+
+
+class TenentsAdmin:
+    def __init__(user: User) -> None:
+        pass
+
+    def verify(self):
+        if (self.user.is_admin):
+            self.db.exec("UPDATE tenents SET verified = %s WHERE user_id = %s", (True, self.user.id))
+            self.db.commit()
+            return True
+        return False
+    def disable():
+        pass
