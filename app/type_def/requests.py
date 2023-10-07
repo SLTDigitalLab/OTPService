8a20b5450b30b586b7eb616d60e570716bfb45dc
Navindu Dananga <navindudananga123@gmail.com>
2023-10-08 02:21:41 +0530

    updates

--- app/type_def/requests.py
+++ app/type_def/requests.py
@@ -2,6 +2,25 @@ from pydantic import BaseModel
 
 class SMSOTPSendRq(BaseModel):
     mobile: str
+    
+class SMSOTPVerifyRq(BaseModel):
+    mobile: str
+    otp: str
+    client_secret: str
+
+class SMSMarketingSendRq(BaseModel):
+    mobile: str
+    msg: str
 
 class EmailOTPSendRq(BaseModel):
     email: str
+    client_secret: str
+
+class EmailOTPVerifyRq(BaseModel):
+    email: str
+    otp: str
+    client_secret: str
+
+class EmailMarketingSendRq(BaseModel):
+    email: str
+    msg: str
--- app/type_def/tenent.py
+++ app/type_def/tenent.py
@@ -4,7 +4,7 @@ from type_def.auth import User
 
 class Tenent(BaseModel):
     id: str
-    owner: User
+    user_id: str
     name: str
     email_otp_size: int = 6
     email_otp_expired_in_s: int = 300
@@ -15,5 +15,28 @@ class Tenent(BaseModel):
     sms_otp_template: str = (
         "Your OTP is {otp}. It's expired at {expired_at}. (Created: {created_at}))"
     )
+    email_otp_template: str = (
+        "Your OTP is {otp}. It's expired at {expired_at}. (Created: {created_at}))"
+    )
+    verified: bool = False
+    disabled: bool = False
     created_ts: datetime | None = None
     updated_ts: datetime | None = None
+
+class TenentRq(BaseModel):
+    name: str
+    email_otp_size: int = 6
+    email_otp_expired_in_s: int = 300
+    email_otp_max_tries: int = 3
+    sms_otp_size: int = 6
+    sms_otp_expired_in_s: int = 300
+    sms_otp_max_tries: int = 3
+    sms_otp_template: str = (
+        "Your OTP is {otp}. It's expired at {expired_at}. (Created: {created_at}))"
+    )
+    email_otp_template: str = (
+        "Your OTP is {otp}. It's expired at {expired_at}. (Created: {created_at}))"
+    )
+
+class TenentRenameRq(BaseModel):
+    name: str
