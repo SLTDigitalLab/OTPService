4fbd8beb16271e88c4fd0bae51bf554c56aa8f93
Navindu Dananga <navindudananga123@gmail.com>
2023-10-08 02:21:07 +0530

    email service configured

--- app/api/v1/email/send_otp.py
+++ app/api/v1/email/send_otp.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta
 from services.email import send_email
 from db.psql_connector import DB, default_config
 from libs.otp import gen_otp
+from uuid import uuid4
 
 from type_def.auth import User
 from type_def.tenent import Tenent
@@ -18,24 +19,15 @@ def send_email_otp(
     otp = gen_otp(tenent.email_otp_size)
     created_at = datetime.now()
     expired_at = datetime.now() + timedelta(seconds=tenent.email_otp_expired_in_s)
-    print(
-        tenent.id,
-        user.id,
-        send_to,
-        otp,
-        created_at,
-        expired_at,
-        False,
-        None,
-        0,
-    )
+    secret = str(uuid4())
     db.exec(
-        "INSERT INTO email_otp (tenent, user_id, sent_to, otp, created_at, expired_at, validated, validated_at, tries) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s) RETURNING (id, tenent, user_id, sent_to, created_at, expired_at)",
+        "INSERT INTO email_otp (tenent_id, user_id, sent_to, otp, client_secret, created_at, expired_at, validated, validated_at, tries) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s) RETURNING (id, tenent, user_id, sent_to, created_at, expired_at)",
         (
             tenent.id,
             user.id,
             send_to,
             otp,
+            secret,
             created_at,
             expired_at,
             False,
@@ -45,8 +37,6 @@ def send_email_otp(
     )
 
     result = db.fetchone()
-    print(result)
-
     try:
         send_email(
             str(tenent.name),
@@ -59,7 +49,7 @@ def send_email_otp(
         return Success(
             "Verification email sent",
             200,
-            {"send_to": send_to, "created_at": created_at, "expired_at": expired_at},
+            {"send_to": send_to, "client_secret": secret, "created_at": created_at, "expired_at": expired_at},
         )
     except Exception as e:
         return Error("Failed to send verification email. Reason: " + str(e), 1000, 400)
--- app/api/v1/email/verify_otp.py
+++ app/api/v1/email/verify_otp.py
@@ -1,4 +1,44 @@
 from type_def.tenent import Tenent
+from db.psql_connector import DB, default_config
+from type_def.common import Success, Error
+from datetime import datetime
 
-def verify_email_otp(tenent: Tenent, otp, email):
-    pass
+def verify_email_otp(tenent: Tenent, otp: str, mobile_number: str, client_secret: str) -> Success | Error:
+    db = DB(default_config())
+    db.exec(
+        "SELECT * FROM email_otp WHERE tenent_id = %s AND sent_to = %s AND client_secret = %s LIMIT 1",
+        (
+            tenent.id,
+            mobile_number,
+            otp,
+            client_secret,
+        ),
+    )
+    result = db.fetchone()
+    if result:
+        real_otp = result.get('otp')
+        if real_otp and real_otp == str(otp):
+            if result.get('validated'):
+                return Error("OTP is already validated.", 1000, 400)
+            elif result.get('expired_at') and result.get('expired_at') < datetime.now():
+                return Error("OTP is expired.", 1000, 400)
+            elif result.get('tries') and result.get('tries') >= tenent.sms_otp_max_tries:
+                return Error("OTP is expired.", 1000, 400)
+            else:
+                db.exec("UPDATE email_otp SET validated = %s, validated_at = %s WHERE id = %s",
+                    (
+                        True,
+                        datetime.now(),
+                        result.get('id'),
+                    )
+                )
+                return Success("OTP verified.", 200, result)
+        else:
+            db.exec("UPDATE email_otp SET tries = %s WHERE id = %s",
+                (
+                    result.get('tries') + 1,
+                    result.get('id'),
+                )
+            )
+            return Error("Invalid OTP.", 1000, 400)
+    return Error("Invalid request, please check client secret and the email is valid", 1000, 400)
