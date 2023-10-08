db6f5be4f0d97867e2a0fb15de1149538dd62657
Navindu Dananga <navindudananga123@gmail.com>
2023-10-09 00:31:28 +0530

    OTP expiring bug fixed

--- app/api/v1/sms/verify_otp.py
+++ app/api/v1/sms/verify_otp.py
@@ -3,7 +3,10 @@ from db.psql_connector import DB, default_config
 from type_def.common import Success, Error
 from datetime import datetime
 
-def verify_sms_otp(tenent: Tenent, otp: str, mobile: str, client_secret: str) -> Success | Error:
+
+def verify_sms_otp(
+    tenent: Tenent, otp: str, mobile: str, client_secret: str
+) -> Success | Error:
     db = DB(default_config())
     db.exec(
         "SELECT * FROM sms_otp WHERE tenent_id = %s AND sent_to = %s AND client_secret = %s LIMIT 1",
@@ -15,30 +18,47 @@ def verify_sms_otp(tenent: Tenent, otp: str, mobile: str, client_secret: str) ->
     )
     result = db.fetchone()
     if result:
-        real_otp = result.get('otp')
+        print(result)
+        real_otp = result.get("otp")
         if real_otp and real_otp == str(otp):
-            if result.get('validated'):
+            if result.get("validated"):
                 return Error("OTP is already validated.", 1000, 400)
-            elif result.get('expired_at') and  (datetime.timestamp(datetime.now()) - datetime.timestamp(result.get('expired_at'))) <= 0:
+            elif (
+                result.get("expired_at")
+                and (
+                    datetime.timestamp(result.get("expired_at"))
+                    - datetime.timestamp(datetime.now())
+                )
+                <= 0
+            ):
                 return Error("OTP is expired.", 1000, 400)
-            elif result.get('tries') and result.get('tries') >= tenent.sms_otp_max_tries:
+            elif (
+                result.get("tries") and result.get("tries") >= tenent.sms_otp_max_tries
+            ):
                 return Error("OTP is expired.", 1000, 400)
             else:
-                x = db.exec("UPDATE sms_otp SET validated = %s, validated_at = %s WHERE id = %s",
+                x = db.exec(
+                    "UPDATE sms_otp SET validated = %s, validated_at = %s WHERE id = %s",
                     (
                         True,
                         datetime.now(),
-                        result.get('id'),
-                    )
+                        result.get("id"),
+                    ),
                 )
                 db.commit()
                 return Success("OTP verified.", 200, result)
         else:
-            db.exec("UPDATE sms_otp SET tries = %s WHERE id = %s",
+            db.exec(
+                "UPDATE sms_otp SET tries = %s WHERE id = %s",
                 (
-                    result.get('tries') + 1,
-                    result.get('id'),
-                )
+                    result.get("tries") + 1,
+                    result.get("id"),
+                ),
             )
+            db.commit()
             return Error("Invalid OTP.", 1000, 400)
-    return Error("Invalid request, please check client secret and the mobile number is valid", 1000, 400)
+    return Error(
+        "Invalid request, please check client secret and the mobile number is valid",
+        1000,
+        400,
+    )
